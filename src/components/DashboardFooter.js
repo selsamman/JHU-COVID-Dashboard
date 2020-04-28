6324d30437246d9bb04429819ad7bbb72fd2c928
Sam Elsamman <sam.elsamman@gmail.com>
2020-04-28 14:36:02 +0100

    New Features Deployed to Prod

--- src/components/DashboardFooter.js
+++ src/components/DashboardFooter.js
@@ -4,7 +4,7 @@ export const DashboardFooter = ({mode}) =>  {
     const fontStyle = {fontSize: mode === 'table' ? 12 : 8};
     return (
         <Navbar bg="dark" variant="dark" sticky="bottom">
-            <Navbar.Brand><span style={{display: "inline-block", ...fontStyle, marginTop: -10, textTransform: "uppercase", fontWeight: "bold"}}>&copy; 2020 magixIT LLC V1.04</span></Navbar.Brand>
+            <Navbar.Brand><span style={{display: "inline-block", ...fontStyle, marginTop: -10, textTransform: "uppercase", fontWeight: "bold"}}>&copy; 2020 magixIT LLC V1.1</span></Navbar.Brand>
              <Navbar.Collapse className="justify-content-end">
                  <Nav >
                      <Nav.Link href="https://coronavirus.jhu.edu/" target="_blank" style={fontStyle}>Data from John's Hopkins</Nav.Link>
